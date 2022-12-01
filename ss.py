8bd57eb5d73747dd43b41da4be65cc372e34543f
scotter567 <scotter567@gmail.com>
2022-11-30 23:20:54 -0500

    Update Readme

--- ss.py
+++ ss.py
@@ -3,9 +3,6 @@ import sys
 HOST = socket.gethostname()
 PORT = 5000
 
-arr = [1,2,3,4,5,6,7,8,9]
-print(arr[-3:])
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
