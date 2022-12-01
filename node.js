018a05df052dd59f8f608254762f5a8928c7ca83
scotter567 <scotter567@gmail.com>
2022-11-30 23:54:32 -0500

    added node file

---
+++ node.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const fs = require('fs');
+const net = require('net')
+const app = express();
+
+
+app.set('view engine','ejs');
+
+
+app.get('/', function(req, res){
+	res.write("<a href = '/watch/owl-house/1'>Vid1</a>")
+	res.write("<br>")
+	res.write("<a href = '/watch/owl-house/2'>Vid2</a>")
+
+})
+
+app.get('/watch/:title/:ep', function (req, res) {
+	console.log(req.params.title, req.params.ep);
+	let path = "./shows/"+req.params.title+"/"+req.params.ep
+	const stat = fs.statSync(path)
+	const fileSize = stat.size
+	const range = req.headers.range
+	if (range) {
+		const parts = range.replace(/bytes=/, "").split("-")
+		const start = parseInt(parts[0],10);
+		const end = parts[1] ? parseInt(parts[1],10) : fileSize-1
+		const chunksize = (end-start)+1
+		const file = fs.createReadStream(path, {start, end})
+		let msg = 'RES: 206,' + 'BYTES: ' + start + '-' + end + " SENDER: 192.168.1.98"
+		var client = new net.Socket();
+		client.connect(5000, '192.168.1.70', function() {
+				console.log('Connected');
+    		    client.write(msg);
+    		    client.destroy()
+		});
+		console.log(msg)		
+		const head = {
+			'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+			'Accept-Ranges': 'bytes',
+			'Content-Length': chunksize,
+			'Content-Type': 'video/mp4',
+		}
+		res.writeHead(206, head)
+		file.pipe(res)		
+		//await sendMsg('192.168.1.70', 5000, msg)
+	} else {
+		let msg = "RES: 200 "+"SENDER: 192.168.1.98 COMPLETE"
+		var client = new net.Socket();
+		client.connect(5000, '192.168.1.70', function() {
+   		    console.log('Connected');
+    		    client.write(msg);
+    		    client.destroy()
+		});
+		console.log(msg)
+		const head = {
+			'Content-Length': fileSize,
+			'Content-Type': 'video/mp4',
+		}
+		res.writeHead(200, head)
+		fs.createReadStream(path).pipe(res)
+	}
+})
+
+app.listen('3000');
